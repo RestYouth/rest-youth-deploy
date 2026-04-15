ca60768b45471844b0fc6116d133a6fe1b41c186
NK Studio <unity_goinmool@kakao.com>
2026-04-16 02:36:05 +0900

    change br

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-RestYouth-0.1.0";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js.gz",
-    "Build/Build.data.gz",
-    "Build/Build.wasm.gz",
+    "Build/Build.framework.js.br",
+    "Build/Build.data.br",
+    "Build/Build.wasm.br",
     "TemplateData/style.css"
 
 ];
